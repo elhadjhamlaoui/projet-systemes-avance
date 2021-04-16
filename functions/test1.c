9428803c5510753f60bfe914184bf9f8ab0992e8
elhadj <hamlaouielhadj@gmail.com>
2021-04-16 12:28:16 +0200

    update client functions

--- functions/test1.c
+++ functions/test1.c
@@ -5,33 +5,32 @@
 int test1(LpcArg *args)
 {
 
-    FILE *fp;
+    // FILE *fp;
 
-    fp = fopen(" GeeksForGeeks.txt ", "r");
+    // fp = fopen(" GeeksForGeeks.txt ", "r");
 
-    if (fp == NULL)
-    {
-        return -1;
-    }
-
-    // int i = 0;
-    // while (args[i].type != NOP)
+    // if (fp == NULL)
     // {
-    //     switch (args[i].type)
-    //     {
-    //     case INT:
-    //         args[i].intg = args[i].intg * 2;
-    //         break;
-    //     case DOUBLE:
-    //         args[i].dbl = args[i].dbl * 2;
-    //         break;
-    //     case STRING:
-    //         strcat(args[i].str.string, " **modified");
-    //         break;
-    //     default:
-    //         break;
-    //     }
-    //     i++;
+    //     return -1;
     // }
-    //exit(0);
+
+    int i = 0;
+    while (args[i].type != NOP)
+    {
+        switch (args[i].type)
+        {
+        case INT:
+            args[i].intg = args[i].intg * 2;
+            break;
+        case DOUBLE:
+            args[i].dbl = args[i].dbl * 2;
+            break;
+        case STRING:
+            strcat(args[i].str.string, " **modified");
+            break;
+        default:
+            break;
+        }
+        i++;
+    }
 }
